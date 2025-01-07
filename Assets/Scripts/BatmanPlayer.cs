68e0b6cdb043d83aef523ace688a75d950c2f2eb
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-07 18:28:12 +0100

    AnimationsBatman

--- Assets/Scripts/BatmanPlayer.cs
+++ Assets/Scripts/BatmanPlayer.cs
@@ -7,11 +7,13 @@ public class BatmanPlayer : MonoBehaviour
     public float upVel = 300f;
     private Rigidbody2D batmanRb;
     private bool isDead;
+    private Animator batmanAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         batmanRb = GetComponent<Rigidbody2D>();
+        batmanAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,14 +21,25 @@ public class BatmanPlayer : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && !isDead)
         {
-            batmanRb.velocity = Vector2.zero;
-            batmanRb.AddForce(Vector2.up * upVel);
+            Flap(); 
         }
     }
+
+    private void Flap()
+    {
+        batmanRb.velocity = Vector2.zero;
+        batmanRb.AddForce(Vector2.up * upVel);
+        batmanAnimation.SetTrigger("Flap");
+    }
+
+
+
+
 
     private void OnCollisionEnter2D()
     {
         isDead = true;
+        batmanAnimation.SetTrigger("Dead");
     }
 
 }
