0198f38e2b063ded641e964b8164d9e67d069594
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-01-14 19:50:36 +0100

    ExitButtonAdded

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -8,4 +8,16 @@ public class ButtonFunctions : MonoBehaviour
     {
         GameManager.Instance.ChangeScene(sceneName);
     }
+
+    public void ExitApplication()
+    {
+        // Salir de la aplicación
+#if UNITY_EDITOR
+        // Si estamos en el editor, detener la ejecución de Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Si estamos en una plataforma compilada (como Android, Windows, etc.), cerrar la aplicación
+            Application.Quit();
+#endif
+    }
 }
